04215c4d059edc83b7f914d167086c5ebed4e49b
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2025-01-06 16:34:44 +0530

    Added Props

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import Home from './Home';
-import { Link } from 'react-router-dom';
+import Contact from './Contact';
+import Header from './header';
 
-const About = () => {
+function About (){
+    let user = {
+        name: 'John Doe',
+        age: 25,
+        hobbies: ['Reading', 'Traveling', 'Gaming'],
+      };
+
+      const message= () =>{
+        console.log("Message");  
+        addMessage();      
+      }
+
+      const addMessage = () =>{
+    
+        console.log("Add Message");
+        
+      }
+
     return (
         <div>
-            {/* <Home /> */}
-            <Link to="/">
-            <button >Go To HOME</button>
-            </Link>
+            <Header/>
             <h1>About Page</h1>
             <p>This is the about page of our React Router example.</p>
+            {/* <Contact name={user.name}/>
+            <Contact name="Sample 2"/> */}
+            <Contact  user={user} name={user.name} methodName={message}/>
         </div>
     );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import Header from './header';
 
-const Contact = () => {
+const Contact = (props) => {
+    console.log(props);
+    
     return (
         <div>
-            <h1>Contact Page</h1>
+            <Header/>
+            <h1>Contact {props.name}</h1>
             <p>Get in touch with us through this contact page.</p>
+            <div>
+          <h2>Hello, {props.name}!</h2>
+          <p>You are {props.age} years old.</p>
+          <h3>Your Hobbies:</h3>
+          <ul>
+            {props.user.hobbies.map((hobby, index) => (
+              <li key={index}>{hobby}</li>
+            ))}
+          </ul>
+
+          <button onClick={props.methodName}>Click Me</button>
+        </div>
+
         </div>
     );
+    
 };
 
 export default Contact;
